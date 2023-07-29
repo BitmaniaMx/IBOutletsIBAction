7b39af40c64ce17c87d435842441b5928f318e8a
rafaelgonzalez <rafael.glez@gmail.com>
2023-07-28 18:45:45 -0600

    Initial Commit

---
+++ IBOutletsIBAction/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IBOutletsIBAction
+//
+//  Created by Rafael González on 28/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
